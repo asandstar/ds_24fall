d35e110524243410dd7546d0d75e4e2a20d6a2dd
asandstar <aiziqing2020@163.com>
2024-03-27 20:19:55 +0800

    2rd

--- 06.c
+++ 06.c
@@ -3,57 +3,66 @@
 
 typedef struct
 {
-    int id;   // 进程ID
-    int data; // 剩余数据量
+    int id;        // 进程ID
+    int data;      // 剩余数据量
+    int lastStart; // 最后一次开始处理的时间，用于排序输出
 } Process;
 
-// 初始化进程队列
 void initProcesses(Process **processes, int n)
 {
     *processes = (Process *)malloc(n * sizeof(Process));
     for (int i = 0; i < n; i++)
     {
-        (*processes)[i].id = i + 1;           // 设置进程ID
-        scanf("%d", &((*processes)[i].data)); // 读取每个进程的数据量
+        scanf("%d", &((*processes)[i].data));
+        (*processes)[i].id = i + 1;
+        (*processes)[i].lastStart = -1; // 初始化时未处理
     }
 }
 
-// 处理进程传输
+int compare(const void *a, const void *b)
+{
+    Process *processA = (Process *)a;
+    Process *processB = (Process *)b;
+    return processA->lastStart - processB->lastStart; // 按最后处理时间排序
+}
+
 void processTransfers(Process *processes, int n, int s, float T)
 {
     float currentTime = 0.0;
+    int processOrder = 0; // 用于记录处理顺序
     while (currentTime < T)
     {
         for (int i = 0; i < n && currentTime < T; i++)
         {
             if (processes[i].data > 0)
-            {                                                                                      // 如果进程还有数据需要传输
-                float transferTime = (processes[i].data < s) ? (float)processes[i].data / s : 1.0; // 计算传输所需时间
+            {
+                float transferTime = processes[i].data < s ? (float)processes[i].data / s : 1.0;
                 if (currentTime + transferTime > T)
-                { // 如果当前时间加上传输时间超出总时间，调整传输时间
+                {
                     transferTime = T - currentTime;
                 }
-                int dataTransferred = s * transferTime; // 计算这次传输的数据量
-                processes[i].data -= dataTransferred;   // 更新剩余数据量
-                currentTime += transferTime;            // 更新当前时间
+                processes[i].data -= s * transferTime;
+                processes[i].lastStart = processOrder++; // 更新处理顺序
+                currentTime += transferTime;
             }
         }
     }
+    // 根据最后一次处理时间对进程进行排序
+    qsort(processes, n, sizeof(Process), compare);
 }
 
-// 打印最终进程状态
 void printProcesses(Process *processes, int n)
 {
-    int allTransferred = 1;
+    int printed = 0;
     for (int i = 0; i < n; i++)
     {
         if (processes[i].data > 0)
         {
             printf("%d %d\n", processes[i].id, processes[i].data);
-            allTransferred = 0;
+            printed = 1;
         }
     }
-    if (allTransferred)
+    if (!printed)
     {
         printf("DONE\n");
     }
